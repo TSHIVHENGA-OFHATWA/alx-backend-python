0a4a61b3fb9dda408f07bb0e70698b43ca7bdbe3
ofhatwa <tofhatwa@gmail.com>
2024-10-10 21:03:22 +0200

    Adding the python file that defines variables with specifics values

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Module for converting a float to a string.
+Module for defining annotated variables.
 """
 
-
-def to_str(n: float) -> str:
-    """Returns the string representation of a float."""
-    return str(n)
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
