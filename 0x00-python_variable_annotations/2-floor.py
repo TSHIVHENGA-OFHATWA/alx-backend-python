bb2134bbd98e2ad78c1c6204d3220757cae2277d
ofhatwa <tofhatwa@gmail.com>
2024-10-10 20:56:47 +0200

    Adding the basic annotations python code file that uses floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for getting the floor value of a float.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """Returns the floor value of a float."""
+    return math.floor(n)
