ae79fd3ee6cef4eb8e7e19f26c7057921f218ee2
ofhatwa <tofhatwa@gmail.com>
2024-10-10 20:51:06 +0200

    Removing unused files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module for concatenating two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
